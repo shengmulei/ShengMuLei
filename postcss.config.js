3163dc29aa13a24555ba3a5c172f926feb9cb75b
shengmulei <149875722+shengmulei@users.noreply.github.com>
2025-04-05 18:29:44 +0800

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -4,3 +4,10 @@ module.exports = {
     autoprefixer: {}
   }
 }
+// 假设有 articles 数据
+const articles = [
+  { title: "文章1", url: "/article1" },
+  { title: "文章2", url: "/article2" }
+];
+
+const list = `<ul>${articles.map(a => `<li><a href="${a.url}">${a.title}</a></li>`).join('')}</ul>`;
